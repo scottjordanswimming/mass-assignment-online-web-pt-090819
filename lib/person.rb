3020eef1ceeb80cb092e6cacebc8a35d566c634f
Scott Jordan <scottjordanswimming@gmail.com>
2019-10-21 13:09:00 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
   
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight
   
   
  def initialize(attributes)
